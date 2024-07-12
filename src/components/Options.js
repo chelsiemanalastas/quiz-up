8249926a72030a830fc1c0783397d0d31e3cdd55
Chelsie Manalastas <manalastaschelsie@gmail.com>
2024-07-12 14:27:32 +0800

    Handling highscore and quiz restart

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,9 +5,11 @@ function Options({ question, dispatch, answer }) {
       {question.options.map((option, index) => (
         <button
           className={`btn btn-option ${index === answer ? "answer" : ""} ${
-            hasAnswered && index === question.correctOption
-              ? "correct"
-              : "wrong"
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
           }`}
           key={option}
           disabled={hasAnswered}
